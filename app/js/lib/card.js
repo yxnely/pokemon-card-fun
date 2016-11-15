944062a55c40a4b616d9b3017fbe2265a5cd1459
Yanely <yanely_rmz@yahoo.com>
2016-11-14 23:11:07 -0500

    Abilities are displaying on card

--- app/js/lib/card.js
+++ app/js/lib/card.js
@@ -9,35 +9,69 @@
         $.get(url)
             .done(function (res) {
                 var pokemon = handleData(res);
-                pokemonContainer.append(pokemon);
             });
     }
 
     function handleData(res) {
+        var collectedName = pokeName(res);
         var pokemonImage = getImage(res.forms[0].url);
-        console.log(res.forms[0].url);
+        pokemonContainer.append(collectedName);
+    }
+
+    function pokeName (res) {
+        var pokeAbilities = res.abilities;
+        var abilityName = '';
+        var pokemonStats = '';
+        //console.log(pokeAbilities);
 
         var pokemonName = $("<h1></h1>", {
-            text: res.name
-        });
-
-        var pokemonWeight = $("<h2></h2>", {
-            text: res.weight
+            text: res.name,
+            class: 'pokemon__name'
         });
 
         var div = $("<div></div>", {
             class: "col-4"
         })
-            .append(pokemonName)
-            .append(pokemonWeight);
+            .append(pokemonName);
+
+        $.each(pokeAbilities, function (idx, val) {
+            abilityName = val.ability.name;
+            pokemonStats = pokestats(abilityName, res.name);
+            div.append(pokemonStats);
+        });
+
+        //console.log(div);
+        div[0].dataset.pokemonName = res.name;
 
         return div;
+    }
+
+    function pokestats (val, name) {
+        var item = $("<h2></h2>", {
+            text: val
+        });
+
+        var container = $("<div></div>", {
+            class: "pokemon__list"
+        }).append(item);
+
+        return container;
     }
 
     function getImage (url) {
         $.get(url)
             .done(function (res) {
-                console.log(res);
+                createImage(res);
             });
+    }
+
+    function createImage (res) {
+        var image = $("<img />", {
+            class: 'pokemon__image',
+            src: res.sprites.front_default
+        });
+
+        var targetDiv = $(".row").find("[data-pokemon-name='" + res.name + "']");
+        targetDiv.prepend(image);
     }
 }(jQuery));
